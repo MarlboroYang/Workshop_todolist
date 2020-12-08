db4671cb49c929853f52519c4421ac6c7d49fff3
MarlboroYang <yangmarlboro@gmail.com>
2020-12-08 16:38:44 +0800

    add delete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,7 +1,14 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function(){
-  const List = document.querySelector('ul')
-  List.addEventListener('click', function(e){
+  const list = document.querySelector('ul')
+  list.addEventListener('click', function(e){
     e.target.classList.toggle('checked')
+  })
+  list.addEventListener('click', function(event){
+    const li = event.target.parentElement
+    console.log(event.target)
+    if (event.target.classList.contains('close')){
+      li.remove()
+    }
   })
 })
